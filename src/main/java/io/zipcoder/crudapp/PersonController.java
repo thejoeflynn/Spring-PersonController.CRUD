63d909e028190cb66baf899bc8aa5f094b3a863e
thejoeflynn <thejoeflynn@gmail.com>
2026-06-11 13:51:46 -0400

    Part 2

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -2,10 +2,8 @@ package io.zipcoder.crudapp;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,36 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
-    private final Map<Integer, Person> people = new ConcurrentHashMap<>();
-    private final AtomicInteger idCounter = new AtomicInteger(0);
+    @Autowired
+    private PersonRepository repo;
 
     @PostMapping("/people")
     public Person createPerson(@RequestBody Person p) {
-        int id = idCounter.incrementAndGet();
-        p.setId(id);
-        people.put(id, p);
-        return p;
+        return repo.save(p);
     }
 
     @GetMapping("/people")
     public List<Person> getPersonList() {
-        return new ArrayList<>(people.values());
+        List<Person> list = new ArrayList<>();
+        repo.findAll().forEach(list::add);
+        return list;
     }
 
     @GetMapping("/people/{id}")
     public Person getPerson(@PathVariable int id) {
-        return people.get(id);
+        return repo.findOne(id);
     }
 
     @PutMapping("/people/{id}")
     public Person updatePerson(@RequestBody Person p, @PathVariable int id) {
         p.setId(id);
-        people.put(id, p);
-        return p;
+        return repo.save(p);
     }
 
     @DeleteMapping("/people/{id}")
     public void deletePerson(@PathVariable int id) {
-        people.remove(id);
+        repo.delete(id);
     }
 }
